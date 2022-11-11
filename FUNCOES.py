e38f1ed22ae505405f0213fdd02b68088b1d5eee
Lucas Filizola <lucasfpp@al.insper.edu.br>
2022-11-11 11:22:48 -0300

    funcao2

--- FUNCOES.py
+++ FUNCOES.py
@@ -55,6 +55,94 @@ def sorteia_questao_inedida(dicionario,nivel,lista_sort):
     lista_sort.append(questao_sorteada)
     return questao_sorteada
 
+def valida_questao(dic):
+    new = {}
+    answer = ['A','B','C','D']
+    aa = 0
+    bb = 0
+    cc = 0
+    dd = 0
+
+    if len(dic) != 4:
+        if 'titulo' not in dic:
+            new['titulo'] = 'nao_encontrado'
+        if 'nivel' not in dic:
+            new['nivel'] = 'nao_encontrado'
+        if 'opcoes' not in dic:
+            new['opcoes'] = 'nao_encontrado'
+        if 'correta' not in dic:
+            new['correta'] = 'nao_encontrado'
+        new['outro'] = 'numero_chaves_invalido'
+    if 'correta' in dic:
+        if dic['correta'] not in answer:
+            new['correta'] = 'valor_errado'
+    if 'nivel' in dic:
+        if dic['nivel'] not in ['facil','medio','dificil']:
+            new['nivel'] = 'valor_errado'
+    if 'titulo' in dic:
+        if dic['titulo'].strip() == '':
+            new['titulo'] = 'vazio'
+    if 'opcoes' in dic:
+        new2 = {}
+        if len(dic['opcoes']) != 4:
+            new['opcoes'] = 'tamanho_invalido'
+        else:
+            if 'A' in dic['opcoes']:
+                if dic['opcoes']['A'].strip() == '':
+                    new2['A'] = 'vazia'
+                    aa = 1
+            else:
+                new2['A'] = 'nao_encontrada'
+                aa = 1
+            if 'B' in dic['opcoes']:
+                if dic['opcoes']['B'].strip() == '':
+                    new2['B'] = 'vazia'
+                    bb = 1
+            else:
+                new2['B'] = 'nao_encontrada'
+                bb = 1
+            if 'C' in dic['opcoes']:
+                if dic['opcoes']['C'].strip() == '':
+                    new2['C'] = 'vazia'
+                    cc = 1
+            else:
+                new2['C'] = 'nao_encontrada'
+                cc = 1
+            if 'D' in dic['opcoes']:
+                if dic['opcoes']['D'].strip() == '':
+                    new2['D'] = 'vazia'
+                    dd = 1
+            else:
+                new2['D'] = 'nao_encontrada'
+                dd = 1
+            if len(new2) != 0:
+                new['opcoes'] = new2
+    if len(new) == 0:
+        return {}
+    else:
+        return new
+
+def questao_para_texto(quest,n):
+    x = f'''----------------------------------------
+QUESTAO {n}
+
+{quest['titulo']}
+
+RESPOSTAS:
+A: {quest['opcoes']['A']}
+B: {quest['opcoes']['B']}
+C: {quest['opcoes']['C']}
+D: {quest['opcoes']['D']}'''
+    return x
+
+
+
+def valida_questoes(question):
+    new = []
+    for i in question:
+        new.append(valida_questao(i))
+    return new
+
 
 
     
